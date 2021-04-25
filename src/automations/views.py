49843fae9e7180b25cc53bf9890c43e31dbe7a11
Fabian Braun <fsbraun@gmx.de>
2021-04-25 08:00:03 +0200

    Rename once more

---
+++ src/automations/views.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+# Create your views here.
+from django.views import View
+
+
+class TaskView(View):
+    pass
+
+class TaskListView(View):
+    pass
+
+class TaskDashboard(View):
+    pass
