bece581dbc8a08ccbaeb6ba32c34b4d7d6a8646a
fsbraun <fsbraun@gmx.de>
2021-04-29 07:46:05 +0200

    Delete build/lib directory

--- build/lib/automations/apps.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from django.apps import AppConfig
-from django.utils.translation import gettext as _
-
-
-class AutomationsConfig(AppConfig):
-    name = 'automations'
-    verbose_name = _('Automations')
--- build/lib/automations/settings.py
+++
@@ -1,3 +0,0 @@
-# coding=utf-8
-
-MAX_FIELD_LENGTH = 128
