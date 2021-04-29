82b2c24ffe8388965d199bd5d0e5c3c3438d17a9
Fabian Braun <fsbraun@gmx.de>
2021-04-29 17:28:36 +0200

    Fix:		Naming bug

--- src/automations/urls.py
+++ src/automations/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("<int:task_id>", views.TaskView.as_view(), name="task"),
     path("tasks", views.TaskListView.as_view(), name="task_list"),
-    path("dashboard", views.TaskDashboard.as_view(), name="dashboard"),
+    path("dashboard", views.TaskDashboardView.as_view(), name="dashboard"),
 ]
