b7b70cf6802a0cfa1bebdd1d2a4380dbb661b6de
Fabian Braun <fsbraun@gmx.de>
2021-05-13 17:26:52 +0200

    Add:		CMS_Apphook

--- src/automations/cms_automations/cms_apps.py
+++ src/automations/cms_automations/cms_apps.py
@@ -1,11 +1,15 @@
 # coding=utf-8
 from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
 from django.utils.translation import gettext as _
 
-class FundApphook (CMSApp):
+
+class AutomationsApphook (CMSApp):
     name = _("Django Automations")
-    app_name = 'autmations'
+    app_name = 'automations'
 
     def get_urls(self, page=None, langague=None, **kwargs):
         return ["automations.urls"]
 
+
+apphook_pool.register(AutomationsApphook)
--- src/automations/urls.py
+++ src/automations/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.TaskListView.as_view(), name="task_list"),
     path("<int:task_id>", views.TaskView.as_view(), name="task"),
-    path("tasks", views.TaskListView.as_view(), name="task_list"),
     path("dashboard", views.TaskDashboardView.as_view(), name="dashboard"),
 ]
