52adf56731a6b209a4f22ec6fca78c744ef3d252
Jack Linke <jacklinke@gmail.com>
2021-11-21 19:05:57 -0500

    Modify to use checks framework when checking settings

--- src/automations/apps.py
+++ src/automations/apps.py
@@ -1,8 +1,39 @@
 # coding=utf-8
 from django.apps import AppConfig
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
+from django.core.checks import Error
+from django.core.checks import Tags as DjangoTags
+from django.core.checks import register
 from django.utils.translation import gettext_lazy as _
+
+
+class Tags(DjangoTags):
+    automations_settings_tag = "automations_settings"
+
+
+def checks_atm_settings(app_configs, **kwargs):
+    """Checks that either all or none of the group/permissions settings are set"""
+    errors = []
+    group_and_permission_settings = (
+        hasattr(settings, "ATM_GROUP_MODEL"),
+        hasattr(settings, "ATM_USER_WITH_PERMISSIONS_FORM_METHOD"),
+        hasattr(settings, "ATM_USER_WITH_PERMISSIONS_MODEL_METHOD"),
+    )
+
+    if any(group_and_permission_settings) and not all(group_and_permission_settings):
+        errors = [
+            (
+                Error(
+                    "Django Automations settings incorrectly configured",
+                    hint=_(
+                        "Either all or none of the following settings must be present: ATM_GROUP_MODEL, "
+                        "ATM_USER_WITH_PERMISSIONS_FORM_METHOD, ATM_USER_WITH_PERMISSIONS_MODEL_METHOD"
+                    ),
+                    id="automations.E001",
+                )
+            )
+        ]
+    return errors
 
 
 class AutomationsConfig(AppConfig):
@@ -11,15 +42,5 @@ class AutomationsConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
-        group_and_permission_settings = (
-            hasattr(settings, "ATM_GROUP_MODEL"),
-            hasattr(settings, "ATM_USER_WITH_PERMISSIONS_FORM_METHOD"),
-            hasattr(settings, "ATM_USER_WITH_PERMISSIONS_MODEL_METHOD"),
-        )
-        if any(group_and_permission_settings) and not all(
-            group_and_permission_settings
-        ):
-            raise ImproperlyConfigured(
-                "Either all or none of the following settings must be present: ATM_GROUP_MODEL, "
-                "ATM_USER_WITH_PERMISSIONS_FORM_METHOD, ATM_USER_WITH_PERMISSIONS_MODEL_METHOD"
-            )
+        super().ready()
+        register(Tags.automations_settings_tag)(checks_atm_settings)
