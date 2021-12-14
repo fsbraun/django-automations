665ecfcf2c029caa07fda83aa67f828f46ded46e
Fabian Braun <fsbraun@gmx.de>
2021-12-14 12:44:51 +0100

    Add:		app_name in urls.py

--- src/automations/urls.py
+++ src/automations/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "automations"
+
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="task_list"),
     path("<int:task_id>", views.TaskView.as_view(), name="task"),
